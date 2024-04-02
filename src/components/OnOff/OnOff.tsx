718d00c8738ada486f4268a456e6becc5a9fc631
Anna <blinova.anneta@gmail.com>
2024-04-02 17:33:03 +0200

    update OnOff component

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,40 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-type OnOffPropsType = {
-  value: true | false
-}
+function OnOff() {
+  console.log('OnOff rendering');
 
-function OnOff(props: OnOffPropsType) {
+  let [value, setValue] = useState(false)
+  console.log('value:', value)
+
+  const onStyle = {
+    width: '30px',
+    height: '20px',
+    border: '1px solid black',
+    display: 'inline-block',
+    padding: '2px',
+    backgroundColor: value ? 'green' : 'white'
+  }
+  const offStyle = {
+    width: '30px',
+    height: '20px',
+    border: '1px solid black',
+    display: 'inline-block',
+    padding: '2px',
+    margin: '2px',
+    backgroundColor: value ? 'white' : 'red'
+  }
+  const indicatorStyle = {
+    width: '10px',
+    height: '10px',
+    border: '1px solid black',
+    borderRadius: '50%',
+    display: 'inline-block',
+    padding: '2px',
+    backgroundColor: value ? 'green' : 'red',
+    marginLeft: '5px',
+  }
   return (
-    <>
-    {props.value ? <Button selected={true}/> : <Button selected={false}/>}
-    </>
+    <div>
+      <div onClick={()=>{setValue(true)}} style={onStyle}>on</div>
+      <div onClick={()=>{setValue(false)}} style={offStyle}>off</div>
+      <div style={indicatorStyle}></div>
+    </div>
   )
-}
-
-type ButtonPropsType = {
-  selected: boolean
-}
-function Button (props: ButtonPropsType) {
-  
-      if(props.selected) {
-        return (
-          <div style={{display: 'flex', gap: '20px', alignItems: 'center', marginBottom: '10px'}}>
-            <button style={{backgroundColor: 'green'}}>on</button>
-            <button>off</button>
-            <div style={{backgroundColor: 'green', borderRadius: '50%', width: '15px', height: '15px'}}></div>
-          </div>
-        )     
-      } else {
-        return (
-          <div style={{display: 'flex', gap: '20px', alignItems: 'center'}}>
-            <button>on</button>
-            <button style={{backgroundColor: 'red'}}>off</button>
-            <div style={{backgroundColor: 'red', borderRadius: '50%', width: '15px', height: '15px'}}></div>
-          </div>
-        )
-      }
-  
 }
 
 export default OnOff
