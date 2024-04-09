a7b274452f7f69d96b4ea724cd848fdec871afbf
Anna <blinova.anneta@gmail.com>
2024-04-09 16:17:50 +0200

    updated App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,42 @@
 import React, { useState } from 'react';
 import './App.css';
 import Accordion from './components/Accordion/Accordion';
-import Rating from './components/Rating/Rating';
-import OnOff from './components/OnOff/OnOff';
+import Rating, { RatingValueType } from './components/Rating/Rating';
 import UncontrolledAccordion from './components/UncontrolledAccordion/UncontrolledAccordion';
 import UncontrolledRating from './components/UncontrolledRating/UncontrolledRating';
+import OnOff from './components/OnOff/OnOff';
+import UncontrolledOnOff from './components/UncontrolledOnOff/UncontrolledOnOff';
 
-// export type ValueProps = 'true' | 'false'
+
 
 function App() {
-  console.log('App rendering');
+  // console.log('App rendering');
 
-  // let [value, setValue] = useState<ValueProps>('false')
+  const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+  const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(true)
+  const [value, setValue] = useState(false)
   // console.log('value:', value)
 
   return (
     <div className='App'>
-      {/* <PageTitle title={'This is APP component'}/>
-      <PageTitle title={'My friends'}/> */}
-      {/* Article 1 */}
-      <Rating value={3} />
-      {/* <Accordion titleValue={'Menu'} collapsed={true}/>
-      <Accordion titleValue={'Users'} collapsed={false}/> */}
-      {/* Article 2
-      <Rating value={0}/>
-      <Rating value={1}/>
-      <Rating value={2}/>
-      <Rating value={3}/>
-      <Rating value={4}/>
-      <Rating value={5}/>  */}
-      {/* <OnOff/>
-      <OnOff/> */}
-      <UncontrolledAccordion titleValue={'Menu'}/>
-      {/* <UncontrolledAccordion titleValue={'Users'}/> */}
+      <PageTitle title={'This is APP component'}/>
+      
+      <Rating value={ratingValue} onClick={setRatingValue} />
+
       <UncontrolledRating/>
+
+      <Accordion 
+        titleValue={'Menu'} 
+        collapsed={accordionCollapsed} 
+        onChange={ () => setAccordionCollapsed(!accordionCollapsed) 
+      }/>
+
+      <UncontrolledAccordion titleValue={'Users'}/>
+
+      {/* <OnOff value={value} onChange={() => setValue(!value)}/> */}
+
+      <UncontrolledOnOff onChange={setValue}/> {value.toString()}
+      
     </div>
   );
 }
